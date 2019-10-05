ea2e8cae6cb73e359435c545094c14b96ef442cd
quangln94 <43927626+quangln94@users.noreply.github.com>
2019-10-05 14:55:35 +0700

    Create Install-zabbix-server.sh

---
+++ Task/Script/Zabbix/Install-zabbix-server.sh
@@ -0,0 +1,48 @@
+#!bin/bash
+echo "Install Zabbix-server"
+echo "Step 1: Cai dat cac goi can thiet"
+rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.el7.noarch.rpm
+yum clean all
+yum -y install zabbix-server-mysql zabbix-web-mysql zabbix-agent
+systemctl restart zabbix-server zabbix-agent httpd
+systemctl enable zabbix-server zabbix-agent httpd
+
+echo "Step 2: Install LAMP"
+echo "Step 2.1: Install MariaDB 10.2"
+echo "[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.2/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1" >> /etc/yum.repos.d/MariaDB.repo
+yum -y update
+yum install -y mariadb mariadb-server
+systemctl start mariadb
+systemctl enable mariadb
+mysql_secure_installation
+
+echo "Step 2.2: Install Apache"
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+
+echo "Step 2.3: Install PHP"
+yum install php php-mysql php-gd php-pear –y
+systemctl restart httpd
+echo "Step 2.4: Install WordPress"
+cd /var/www/html/
+wget http://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+cp wordpress/* /var/www/html/
+cp wp-config-sample.php wp-config.php
+
+echo "Step 3: Chi dinh database cho zabbix-server"
+read -p "Ten database: " namedatabase
+sed -i "s/database_name_here/$namedatabase/" /var/www/html/wp-config.php
+read -p "User database: " userdatabase
+sed -i "s/username_here/$userdatabase/" /var/www/html/wp-config.php
+read -p "Password cua User: " passworduser
+sed -i "s/password_here/$passworduser/" /var/www/html/wp-config.php
+read -p "IP cua database: " ipdatabase
+sed -i "s/localhost/$passworduser/" /var/www/html/wp-config.php
+
+chmod +r apache:apache /var/www/html/wp-config.php
